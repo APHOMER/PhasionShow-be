086ab834d8077c062182c9d162f245ff2d164bdb
Aphomer <aaphomer@gmail.com>
2024-06-20 13:38:37 +0100

    initial server.js commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express"); 
+
+const app = express();
+
+
+// const app = require("./app");
+
+const mongoose = require('mongoose');
+
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE || process.env.LOCAL_DATABASE;
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: false
+}).then(() => console.log('DB connection successful.....'));
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}.....`)
+});
+
+// module.exports = app;
