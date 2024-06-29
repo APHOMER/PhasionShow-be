a345268d41d6fa067073f2ac61d0229c02efa707
Aphomer <aaphomer@gmail.com>
2024-06-29 10:45:13 +0100

    installed and implemented body-parser

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express"); 
-
 const app = express();
+
+const bodyParser = require('body-parser');
+
+
 
 
 // const app = require("./app");
 
 const mongoose = require('mongoose');
-
-
 const dotenv = require("dotenv");
-
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE || process.env.LOCAL_DATABASE;
@@ -17,8 +17,17 @@ const DB = process.env.DATABASE || process.env.LOCAL_DATABASE;
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: false
+    useUnifiedTopology: true //false
 }).then(() => console.log('DB connection successful.....'));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json
+app.use(bodyParser.json());
+
+const userRouter = require("./router/userRouter");
+
+app.use("/user", userRouter);
 
 
 
