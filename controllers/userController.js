8a197c554c659a344ded81b5331cd1dc11872cd4
Aphomer <aaphomer@gmail.com>
2024-07-02 11:00:33 +0100

    adding initial controller

---
+++ controllers/userController.js
@@ -0,0 +1,104 @@
+
+const User = require("../models/userModel.js");
+
+
+exports.createUser = async (req, res) => {
+    try {
+        
+        const newUser = await User.create(req.body);
+    
+        res.status(201).json({
+            status: "success",
+            data: {
+                user: newUser
+            }
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            status: "fail",
+            message: error
+        })
+    }
+};
+
+exports.getAllUsers = async (req, res) => { 
+    try { 
+
+        const users = await User.find();    
+        res.status(200).json({
+            status: "success",
+            results: users.length,
+            data: {
+                users
+            }
+        });
+        
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err
+        })
+    } 
+}
+
+exports.getUser = async(req, res) => {
+    try {
+
+        const user = await User.findById(req.params.id);
+        console.log("current user",user);
+        res.status(200).json({
+            status: "success",
+            data: {
+                user
+            }
+        });
+
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err
+        })
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        console.log(req.body);
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: "success",
+            data: {
+                updatedUser
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err
+        })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try {
+        // const deletedUser = await User.findByIdAndDelete(req.params.id);
+        await User.findByIdAndDelete(req.params.id);
+        // console.log(deletedUser);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                deletedUser
+            }
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err
+        });
+    }
+}
