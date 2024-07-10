c02794c9bec9d34b094a0527d84a6ba42868bd81
Aphomer <aaphomer@gmail.com>
2024-07-10 22:36:11 +0100

    added router

---
+++ router/userRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require("../controllers/userController");
+
+router
+    .route('/')
+    .get(userController.getAllUsers)
+    .post(userController.createUser);
+
+router
+    .route('/:id')
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    // .put(userController.updateUser)
+    .delete(userController.deleteUser);
+
+
+
+    module.exports = router;
