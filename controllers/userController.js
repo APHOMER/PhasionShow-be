fb5e01fe4a22290eb01959579c1f1de88a5cfccc
Aphomer <aaphomer@gmail.com>
2024-07-17 08:01:53 +0100

    spacecing

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,6 @@
 const User = require("../models/userModel.js");
 
 
-exports.createUser = async (req, res) => {
-    try {
-        
-        const newUser = await User.create(req.body);
-    
-        res.status(201).json({
-            status: "success",
-            data: {
-                user: newUser
-            }
-        });
-        
-    } catch (error) {
-        res.status(400).json({
-            status: "fail",
-            message: error
-        })
-    }
-};
 
 exports.getAllUsers = async (req, res) => { 
     try { 
