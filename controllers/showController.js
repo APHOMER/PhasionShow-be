324b914be156f682b9e22f65e87ad8b8c77c62e9
Aphomer <aaphomer@gmail.com>
2024-07-19 01:42:04 +0100

    removed dormant Lines

--- controllers/showController.js
+++ controllers/showController.js
@@ -32,17 +32,7 @@ exports.getAllShow = catchAsync(async (req, res, next) => {
 
         })
 
-    // try {
     
-    // } catch (error) {
-
-    //     console.log(error);
-
-    //     res.status(400).json({
-    //         status: "fail",
-    //         message: error
-    //     });
-    // }
 });
 
 
