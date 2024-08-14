ac6ebb876d7a54b0da4e3bc3bc3e0db368a60a8e
Aphomer <aaphomer@gmail.com>
2024-08-14 09:03:54 +0100

    included 'AppError file and GlobalErrorHandler file

--- server.js
+++ server.js
@@ -16,16 +16,40 @@ mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true //false
-}).then(() => console.log('DB connection successful.....'));
+}).then(() => console.log('DB connection successful...'));
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // Parse application/json
 app.use(bodyParser.json());
 
-const userRouter = require("./router/userRouter");
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require("./controllers/errorController"); // Error controller
+const userRouter = require("./router/userRouter");
+// const showRouter = require("./router/showRouter");
+
+// ROUTES
 app.use("/user", userRouter);
+// app.use("show", showRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`can't find ${req.originalUrl} on this server... !`, 404));
+
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `can't find ${req.originalUrl} on this server... !`
+    // });
+
+    // const err = new Error(`can't find ${req.originalUrl} on this server... !`);
+    // err.status = "fail";
+    // err.statusCode = 404;
+
+});
+
+// MIDDLEWARE
+app.use(globalErrorHandler);
+
 
 
 const port = process.env.PORT || 3000;
