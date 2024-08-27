f3043778f8629a6cf37b3fa54ea350c83ebb54f3
Aphomer <aaphomer@gmail.com>
2024-08-27 02:30:58 +0100

    integrated appError Function and Global error function into server.js

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 
 const bodyParser = require('body-parser');
-
-
 // const app = require("./app");
-
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
+
+// UNHANDLED UNCAUGHT EXCEPTION
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION... Shutting down ! !! !!!', );
+    process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const DB = process.env.DATABASE || process.env.LOCAL_DATABASE;
@@ -27,11 +32,20 @@ app.use(bodyParser.json());
 const AppError = require('./utils/appError');
 const globalErrorHandler = require("./controllers/errorController"); // Error controller
 const userRouter = require("./router/userRouter");
-// const showRouter = require("./router/showRouter");
+const showRouter = require("./router/showRouter");
+
+
+// I can't explain the following lines of code
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
+
+    next();
+})
 
 // ROUTES
 app.use("/user", userRouter);
-// app.use("show", showRouter);
+app.use("/show", showRouter);
 
 app.all('*', (req, res, next) => {
     next(new AppError(`can't find ${req.originalUrl} on this server... !`, 404));
@@ -54,9 +68,23 @@ app.use(globalErrorHandler);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App is running on port ${port}.....`)
 });
+
+// UNHANDLED REJECTION PROMISES
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    // console.log(err);
+    console.log('UNHANDLE REJECTION... Shutting down ! !! !!!', )
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+
+
 
 
 // module.exports = app;
