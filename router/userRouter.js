b20c44f3b411243f04a69c828eddce923662e551
Aphomer <aaphomer@gmail.com>
2024-09-03 11:29:24 +0100

    added /updamyPassword route to the router/userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -7,6 +7,17 @@ const authController = require("../controllers/authController");
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
+router.post("/forgotpassword", authController.forgotPassword);
+router.patch("/resetpassword/:token", authController.resetPassword);
+
+router.patch(
+    "/updateMyPassword", 
+    authController.protect, 
+    authController.updatePassword
+);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 // router.route(userController.getUserStats);
 
 router
