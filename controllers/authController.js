8d09d3ee963b997ade9dead601719b5c5fd1455b
Aphomer <aaphomer@gmail.com>
2024-09-30 23:27:19 +0100

    edited newuser in controllers/authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,15 +41,15 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-    // const newUser = await User.create(req.body);
-    const newUser = await User.create({
-        name:req.body.name,
-        email: req.body.email,
-        role: req.body.role,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        passwordChangedAt: req.body.passwordChangedAt
-    });
+    const newUser = await User.create(req.body);
+    // const newUser = await User.create({
+    //     name:req.body.name,
+    //     email: req.body.email,
+    //     role: req.body.role,
+    //     password: req.body.password,
+    //     confirmPassword: req.body.confirmPassword,
+    //     passwordChangedAt: req.body.passwordChangedAt
+    // });
 
     createSendToken(newUser, 201, res);
 
