c7896fc191c0c113575499ea4adb937af61ffd4b
Aphomer <aaphomer@gmail.com>
2024-11-07 20:55:47 +0100

    Uncommented rate limiter

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const app = express();
 const morgan = require('morgan');
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
