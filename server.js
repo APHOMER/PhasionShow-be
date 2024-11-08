9a1e3c6d34cbd795e42acf280096df2c477a4254
Aphomer <aaphomer@gmail.com>
2024-11-08 16:40:33 +0100

    added server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+
 // const hpp = require('hpp');
+
 const cors = require('cors');
 
 
